9e978a60fc45315b4f005b34235092a8fc474df8
Dor Katzir <katzirdor@gmail.com>
2022-04-19 23:49:39 +0300

    mouse controll playing

--- js/slingShot.js
+++ js/slingShot.js
@@ -7,15 +7,15 @@ let render = Matter.Render.create({
     element: document.body,
     engine: engine,
     options: {
-        background: '#fafafa',
+        // background: '#fafafa',
         wireframes: false
     }
 })
 
 // create two boxes and a ground
 let ground = Matter.Bodies.rectangle(400, 610, 810, 60, { isStatic: true })
-let boxA = Matter.Bodies.rectangle(400, 200, 80, 80)
-let boxB = Matter.Bodies.rectangle(450, 50, 80, 80)
+let boxA = Matter.Bodies.rectangle(100, 150, 80, 80)
+let boxB = Matter.Bodies.rectangle(150, 50, 80, 80)
 
 let mouse = Matter.Mouse.create(render.canvas)
 let mouseConstraint = Matter.MouseConstraint.create(engine, {
@@ -29,8 +29,16 @@ let mouseConstraint = Matter.MouseConstraint.create(engine, {
 // add all of the bodies to the world
 Matter.Composite.add(engine.world, [boxA, boxB, ground, mouseConstraint])
 
- // keep the mouse in sync with rendering
-render.mouse = mouse;
+
+// keep the mouse in sync with rendering
+render.mouse = mouse
+
+ // fit the render viewport to the scene
+Matter.Render.lookAt(render, {
+    min: { x: 0, y: 0 },
+    max: { x: 800, y: 600 }
+});
+
 
 // run the renderer
 Matter.Render.run(render)
